d6a239cb1529d68b9fe569cbe8af82aabf939723
JoseNunhez <nunhezmart@gmail.com>
2023-02-08 07:00:19 +0100

    Formularios de registro y login

--- src/pages/loginpage.js
+++ src/pages/loginpage.js
@@ -1,8 +1,37 @@
+import { useState } from 'react';
+import { loginUserService } from '../services';
+
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleForm = async (e) => {
+        e.preventDefault();
+        setError('');
+
+        try {
+            loginUserService({ email, password })
+        } catch (error) {
+            setError(error.message);
+        }
+    }
     return (
         <section>
             <h1>Login</h1>
-            <p>Aquí va un formulario de login</p>
+            <form onSubmit={handleForm}>
+                <fieldset>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" required onChange={(e) => setEmail(e.target.value)} />
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" required onChange={(e) => setPassword(e.target.value)}/>
+                </fieldset>
+
+                <button>Login</button>
+                {error ? <p>{error}</p> : null}
+            </form>
         </section>
     )
 }
--- src/services/index.js
+++ src/services/index.js
@@ -10,3 +10,46 @@ export const loadAllServicesService = async () => {
 
     return json.results;
 };
+
+
+export const registerUserService = async (email, password) => {
+    const response = await fetch((process.env.REACT_APP_API_URL)/'user', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+    });
+
+    const json = await response.json();
+
+    if(!response.ok) {
+        throw new Error(json.message);
+    }
+
+    return json;
+}
+
+export const loginUserService = async (email, password) => {
+    const response = await fetch((process.env.REACT_APP_API_URL)/'auth', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+    });
+
+    const json = await response.json();
+
+    if(!response.ok) {
+        throw new Error(json.message);
+    }
+
+    return json;
+}

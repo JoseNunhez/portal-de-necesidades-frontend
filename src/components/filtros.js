bc7e4705907b8f4fe13e1d2a4730f9eb6c2a5fff
JoseNunhez <nunhezmart@gmail.com>
2023-02-23 11:43:31 +0100

    filtros actualizados

--- src/components/filtros.js
+++ src/components/filtros.js
@@ -4,17 +4,7 @@ export const Filtros = ({ setSort}) => {
     const handleSubmit = (event) => {
         event.preventDefault();
         //printar los valores de los filtros
-        console.log(event.target.value)
-        // if (event.target[0].checked) {
-        //     setSort("publicacion");
-        // } else if (event.target[1].checked) {
-        //     setSort("precio");
-        // } else if (event.target[2].checked) {
-        //     setSort("entrega")
-        // } else {
-        //     setSort("");
-        // }
-
+        setSort(event.target.sortgeneral.value)
     }
 
     return (
@@ -22,9 +12,9 @@ export const Filtros = ({ setSort}) => {
                 <section className="filtros-generales">
                     <h5>Filtros</h5>
                     <form className="filtros-form" onSubmit={handleSubmit}>                
-                        <p>Ordenar por fecha de publicación <input type="radio" name="sortgeneral"  id="sort-publicacion" className="checkmarck" /></p>
-                        <p>Ordenar por precio <input type="radio"name="sortgeneral" id="sort-precio" className="checkmarck" /></p>   
-                        <p>Ordenar por fecha de entrega <input type="radio" name="sortgeneral" id="sort-entrega" className="checkmarck" /></p>
+                        <p>Ordenar por fecha de publicación <input type="radio" value="publicacion" name="sortgeneral"  id="sortpublicacion" className="checkmarck" /></p>
+                        <p>Ordenar por precio <input type="radio"name="sortgeneral" value="precio" id="sortprecio" className="checkmarck" /></p>   
+                        <p>Ordenar por fecha de entrega <input type="radio" value="entrega" name="sortgeneral" id="sortentrega" className="checkmarck" /></p>
                         <button>Filtrar</button>
                     </form>
                 </section>
